76a09681fcf8655988c3eda3642dc7ffd2c78d36
Tama-chan <lolipirate1@gmail.com>
2021-04-23 14:34:34 +0200

    Finished the implementation of step 1

--- src/main/java/com/example/wififingerprint/MainActivity.java
+++ src/main/java/com/example/wififingerprint/MainActivity.java
@@ -1,8 +1,21 @@
 package com.example.wififingerprint;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +24,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void handleButton(View view) {
+        Button myButton = findViewById(R.id.scanWifi);
+        myButton.setEnabled(false);
+        myButton.postDelayed(new Runnable() {
+            Button myButton = findViewById(R.id.scanWifi);
+            @Override
+            public void run() {
+                myButton.setEnabled(true);
+            }
+        }, 30000);
+
+        new CountDownTimer(30000, 1000) {
+            TextView text = findViewById(R.id.countdown);
+
+            public void onTick(long millisUntilFinished) {
+                text.setText("seconds remaining: " + millisUntilFinished / 1000);
+                //here you can have your logic to set text to edittext
+            }
+
+            public void onFinish() {
+                text.setText("done!");
+            }
+
+        }.start();
+
+        Intent intent = new Intent(this, WifiActivity.class);
+        this.startActivity(intent);
+
+    }
+
+
 }
